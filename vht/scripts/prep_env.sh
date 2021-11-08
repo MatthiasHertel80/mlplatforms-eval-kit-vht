24b09948c5f2e5404e42a5b22d63fbb5dc3a559c
Matthias Hertel <matthias.hertel@arm.com>
2021-11-08 11:28:37 +0100

    Update prep_env.sh

--- vht/scripts/prep_env.sh
+++ vht/scripts/prep_env.sh
@@ -1,8 +1,8 @@
-apt update && apt upgrade -y
+# apt update && apt upgrade -y
 
-apt install cmake -y 
+# apt install cmake -y 
 
-apt install python3-venv -y
+# apt install python3-venv -y
 
 cd /home/ubuntu/vhtwork 
 
