a210a23e0cf8f3b50a47d25577d10d5f49cff5d8
Matthias Hertel <matthias.hertel@arm.com>
2021-11-08 10:44:01 +0100

    Update Actions

---
+++ scripts/prep_env.sh
@@ -0,0 +1,18 @@
+apt update && apt upgrade -y
+
+apt install cmake -y 
+
+apt install python3-venv -y
+
+cd /home/ubuntu/vhtwork 
+
+git clone "https://review.mlplatform.org/ml/ethos-u/ml-embedded-evaluation-kit"
+
+cd ml-embedded-evaluation-kit
+
+git submodule update --init
+
+python3 ./set_up_default_resources.py
+
+mkdir build 
+
